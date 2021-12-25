ff17afe5ac450c607c7c31395384d0183bce6f48
filoupegase <loisoncorentin@yahoo.fr>
2021-12-25 18:48:50 +0100

    feature/page login--missing main component

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import { colorStyle } from '../../utils/style/ColorStyle';
 
 
 function Login() {
   return (
-      <p>Login Page</p>
+      <LoginMain>
+        <LoginFormContainer>
+          <i className="fa fa-user-circle" />
+
+          <h2>Sign In</h2>
+
+
+        </LoginFormContainer>
+      </LoginMain>
   );
 }
+
+const LoginMain = styled.main`
+  padding: 3rem;
+  background: ${ colorStyle.secondary.main };`;
+
+const LoginFormContainer = styled.section`
+  width: 20rem;
+  margin: 0 auto;
+  padding: 2rem;
+  border-radius: 0.25rem;
+  background: white;
+  i {
+    color: ${ colorStyle.secondary.main };
+    font-size: 3rem;}`;
 
 export default Login;
---
+++ src/utils/style/ColorStyle.js
@@ -0,0 +1,16 @@
+export const colorStyle = {
+  primary: {
+    main: '#00bc77',
+    primary300: '#00935d',
+    primary600: '#005834'
+  },
+  secondary: {
+    main: '#12002b'
+  },
+  danger500: '#e60000',
+  neutral300: '#ccc',
+  neutral400: '#aaa',
+  neutral700: '#2c3e50',
+  neutral900: '#222',
+  largeWrapper: 'max-width: 90rem;margin: 0 auto;padding: 0 2rem;'
+};
